60a319012fdd22ab60d107d28ab0b7aff30a1a4b
aman <a.kasa@alustudent.com>
2024-06-20 16:37:02 -0700

    no line left

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-number = 98
-print(f"{number} Battery street")
+number = "Holberton"
 
+try:
+    print(f"{int(number)} Battery street")
+except ValueError:
+    print(f"{number} Battery street")
