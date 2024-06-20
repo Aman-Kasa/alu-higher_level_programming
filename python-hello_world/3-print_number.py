613159ef1de78e3280e8c448927c4387f89816f4
aman <a.kasa@alustudent.com>
2024-06-20 16:42:53 -0700

    aha third time

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+import sys
+
 number = "Holberton"
 
 try:
     print(f"{int(number)} Battery street")
-except ValueError:
+except ValueError as e:
     print(f"{number} Battery street")
+    print(f"ValueError: {e}", file=sys.stderr)
+
