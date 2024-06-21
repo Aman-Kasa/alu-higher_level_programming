8a38ecc6abe7eb0863d98bd859f2bd340fb8f5cf
aman <a.kasa@alustudent.com>
2024-06-20 17:00:38 -0700

    fivex

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-import sys
-
-number = "Holberton"
-
-try:
-    print(f"{int(number)} Battery street")
-except ValueError as e:
-    print(f"{number} Battery street")
-    print(f"ValueError: {e}", file=sys.stderr)
+number = 98
+print(f"{number} Battery street")
