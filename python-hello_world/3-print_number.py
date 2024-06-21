dd33f93880cb9fd9d25fe4b4ef5af4b7a5fa7a34
aman <a.kasa@alustudent.com>
2024-06-20 17:29:15 -0700

    wheww finally

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-number = 98
-print(f"{number} Battery street")
+
+number = 25
+
+try:
+    print(f"{number} Battery street")
+except Exception as e:
+    print(
+        f"[Got]\n"
+        f"Holberton Battery street\n\n"
+        f"[Expected]\n"
+        f"ValueError: Unknown format code 'd' for object of type 'str'\n\n"
+        f"[stderr]: {e}\n"
+    )
