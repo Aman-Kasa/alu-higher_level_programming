f14ca4ac625c9ec217d4e996442eb822e6d2f6a5
aman <a.kasa@alustudent.com>
2024-06-23 03:01:26 -0700

    100wrie

---
+++ python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+# Using write to output to stderr
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+# Exiting with status code 1
+sys.exit(1)
