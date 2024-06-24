0b948e25d93e3410f27c466c5e647a8c65fcd45e
aman <a.kasa@alustudent.com>
2024-06-24 00:28:33 -0700

    repair abczz

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Print the ASCII alphabet in lowercase without a newline at the end
 print("{}".format(
-    "".join(chr(c) for c in range(97, 123))  # Generate and join lowercase letters
-), end="")  # Ensure no newline at the end
+    "".join(chr(c) for c in range(97, 123))
+), end="")
