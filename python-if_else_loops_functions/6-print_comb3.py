e4e7da4684f02f1ecd09ec7ca10de6ff3147f1dd
aman <a.kasa@alustudent.com>
2024-06-24 01:19:05 -0700

    restoretri

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}{}".format(i, j, ", " if i < 8 and j < 9 else "\n"), end="")
+        print("{:d}{:d}".format(i, j), end=", " if i < 8 and j < 9 else "\n")
