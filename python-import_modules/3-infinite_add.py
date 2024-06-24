934ce75a3bb339637e48bb974f1a6d5b13781262
aman <a.kasa@alustudent.com>
2024-06-24 05:22:26 -0700

    fndisf

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,14 +6,14 @@ import sys
 def main():
     # Get all arguments from sys.argv excluding the script name
     arguments = sys.argv[1:]
-    
+
     # Initialize a variable to store the sum
     total_sum = 0
-    
+
     # Iterate over each argument, convert it to int and add to total_sum
     for arg in arguments:
         total_sum += int(arg)
-    
+
     # Print the result
     print(total_sum)
 
