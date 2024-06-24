f0fdb72bb0affab09f30a5f32f93ba699ae1b2f0
aman <a.kasa@alustudent.com>
2024-06-24 00:56:15 -0700

    Your commit message here

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        # Check if it's the last pair to decide whether to print ", " or "\n"
-        end = ", " if i < 8 and j < 9 else "\n"
-        print("{:d}{:d}{}".format(i, j, end), end="")
+        print("{:d}{:d}{}".format(i, j, ", " if i < 8 and j < 9 else "\n"), end="")
