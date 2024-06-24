35e8d6daec92e99277e59172138d2e3b7c6cb65c
aman <a.kasa@alustudent.com>
2024-06-24 00:53:10 -0700

    tria

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}{}".format(i, j, ", " if i < 8 and j < 9 else "\n"), end="")
+        # Check if it's the last pair to decide whether to print ", " or "\n"
+        end = ", " if i < 8 and j < 9 else "\n"
+        print("{:d}{:d}{}".format(i, j, end), end="")
