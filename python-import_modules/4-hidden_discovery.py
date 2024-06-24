c35678fed4729d7ce01d9ec21d522692f20e0374
aman <a.kasa@alustudent.com>
2024-06-24 06:30:45 -0700

    games

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
 
-import marshal
 import dis
-
-
-def load_module(file_path):
-    with open(file_path, 'rb') as file:
-        file.seek(16)  # Skip the header
-        code = marshal.load(file)
-    return code
-
-
-def get_names_from_code(code):
-    names = set()
-    for instr in dis.get_instructions(code):
-        if instr.opname == 'LOAD_GLOBAL' and not instr.argval.startswith('__'):
-            names.add(instr.argval)
-    return names
+import marshal
 
 
 def main():
-    code = load_module('hidden_4.pyc')
-    names = get_names_from_code(code)
-    sorted_names = sorted(names)
-    for name in sorted_names:
-        print(name)
+    with open("hidden_4.pyc", "rb") as f:
+        f.seek(16)  # skip the header of the .pyc file
+        code = marshal.load(f)
+
+    for name in sorted(code.co_names):
+        if not name.startswith("__"):
+            print(name)
 
 
 if __name__ == "__main__":
