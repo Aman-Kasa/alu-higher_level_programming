52f1e64e4cf7bb4719d4531f065f03f7b2350509
aman <a.kasa@alustudent.com>
2024-06-25 15:42:55 -0700

    exc maths

--- python-data_structures/6-main.py
+++ python-data_structures/6-main.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
-# Example matrix
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# Function to print a matrix of integers
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        first = True
+        for num in row:
+            if not first:
+                print(" ", end="")
+            else:
+                first = False
+            print("{:d}".format(num), end="")
+        print()
 
-# Print the matrix using the print_matrix_integer function
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+# Test cases
+if __name__ == "__main__":
+    # Example matrices
+    matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    matrix2 = [[1, 2], [4, 5]]
+    matrix3 = [[1, 2], [4, 5], [7, 8]]
+    matrix4 = [[1]]
+    matrix5 = [[1], [2], [3], [4]]
+    matrix6 = [[]]
+
+    # Print each matrix using print_matrix_integer function
+    print_matrix_integer(matrix1)
+    print("--")
+    print_matrix_integer(matrix2)
+    print("--")
+    print_matrix_integer(matrix3)
+    print("--")
+    print_matrix_integer(matrix4)
+    print("--")
+    print_matrix_integer(matrix5)
+    print("--")
+    print_matrix_integer(matrix6)
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         first = True
@@ -9,24 +10,3 @@ def print_matrix_integer(matrix=[[]]):
                 first = False
             print("{:d}".format(num), end="")
         print()
-
-# Test cases
-matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-matrix2 = [[1, 2], [4, 5]]
-matrix3 = [[1, 2], [4, 5], [7, 8]]
-matrix4 = [[1]]
-matrix5 = [[1], [2], [3], [4]]
-matrix6 = [[]]
-
-# Test outputs
-print_matrix_integer(matrix1)
-print("--")
-print_matrix_integer(matrix2)
-print("--")
-print_matrix_integer(matrix3)
-print("--")
-print_matrix_integer(matrix4)
-print("--")
-print_matrix_integer(matrix5)
-print("--")
-print_matrix_integer(matrix6)
