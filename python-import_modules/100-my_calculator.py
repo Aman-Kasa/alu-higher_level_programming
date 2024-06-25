e746399e934c4423fd3e595f537857ab13ee875d
aman <a.kasa@alustudent.com>
2024-06-24 17:19:47 -0700

    work now

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -8,26 +8,34 @@ def calculator():
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    try:
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+    except ValueError:
+        print("Error: Invalid input. <a> and <b> must be integers.")
+        sys.exit(1)
 
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        if b == 0:
-            print("Error: division by zero")
-            sys.exit(1)
-        result = div(a, b)
-    else:
+    if operator not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print(f"{a} {operator} {b} = {result}")
+    try:
+        if operator == '+':
+            result = add(a, b)
+        elif operator == '-':
+            result = sub(a, b)
+        elif operator == '*':
+            result = mul(a, b)
+        elif operator == '/':
+            if b == 0:
+                print("Error: division by zero")
+                sys.exit(1)
+            result = div(a, b)
+        print(f"{a} {operator} {b} = {result}")
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,50 +1,18 @@
+#!/usr/bin/python3
 def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
+    """My addition function"""
+    return a + b
 
 def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
-
+    """My subtraction function"""
+    return a - b
 
 def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
+    """My multiplication function"""
+    return a * b
 
 def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
+    """My division function"""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
     return int(a / b)
