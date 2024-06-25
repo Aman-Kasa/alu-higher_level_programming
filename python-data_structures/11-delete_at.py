cd8e7ef01557fef10defa0a4c28592dd79d4a075
aman <a.kasa@alustudent.com>
2024-06-25 07:20:37 -0700

    line repaired

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -8,12 +8,36 @@ def delete_at(my_list=[], idx=0):
     for i in range(len(my_list)):
         if i != idx:
             new_list.append(my_list[i])
-        return new_list
+            return new_list
 
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     idx = 3
-    new_list = delete_at(my_list, idx)
+    new_list = delete_at(my_list.copy(), idx)
+    print(new_list)
+    print(my_list)
+
+    my_list = [1, 2, 3, 4, 5]
+    idx = 0
+    new_list = delete_at(my_list.copy(), idx)
+    print(new_list)
+    print(my_list)
+
+    my_list = [1, 2, 3, 4, 5]
+    idx = 4
+    new_list = delete_at(my_list.copy(), idx)
+    print(new_list)
+    print(my_list)
+
+    my_list = [1, 2, 3, 4, 5]
+    idx = 5
+    new_list = delete_at(my_list.copy(), idx)
+    print(new_list)
+    print(my_list)
+
+    my_list = []
+    idx = 0
+    new_list = delete_at(my_list.copy(), idx)
     print(new_list)
     print(my_list)
