107bf08939ea944a13699c638e4f16c094c69601
aman <a.kasa@alustudent.com>
2024-06-25 15:28:05 -0700

    matrices

---
+++ python-data_structures/6-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+# Example matrix
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Print the matrix using the print_matrix_integer function
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
