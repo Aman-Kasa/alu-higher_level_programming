36a7145fb8d16a3a1afa8d89f59747442d8ebd4e
aman <a.kasa@alustudent.com>
2024-06-25 15:09:55 -0700

    xagain

--- python-data_structures/10-main.py
+++ python-data_structures/10-main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
 
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
+test_cases = [
+    [1, 2, 3, 4],
+    [0, 1, 2, 3],
+    [0, 1],
+    []
+]
 
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
+for my_list in test_cases:
+    list_result = divisible_by_2(my_list)
+    for i in range(len(list_result)):
+        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    print()  # For better readability of output
