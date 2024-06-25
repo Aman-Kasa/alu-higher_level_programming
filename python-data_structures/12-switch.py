be765df579bf316df7869dd2ce19c09d9114e144
aman <a.kasa@alustudent.com>
2024-06-25 07:27:52 -0700

    5 lines

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
-a, b = 10, 89  # Initial values of a and b
-
-# Switching logic
-# Start of your code
+a, b = 10, 89
 a, b = b, a
-# End of your code
-
 print(f"a={a} - b={b}")
