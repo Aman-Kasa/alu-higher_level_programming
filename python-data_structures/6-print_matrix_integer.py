c2adaae9bc903aaa416f72cac93b0e79c21eae7a
aman <a.kasa@alustudent.com>
2024-06-25 15:31:58 -0700

    matricesss

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,32 @@
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            if i == len(row) - 1:
-                print("{:d}".format(row[i]), end="")
+        first = True
+        for num in row:
+            if not first:
+                print(" ", end="")
             else:
-                print("{:d}".format(row[i]), end=" ")
-        print()  # Move to the next line after each row
+                first = False
+            print("{:d}".format(num), end="")
+        print()
+
+# Test cases
+matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matrix2 = [[1, 2], [4, 5]]
+matrix3 = [[1, 2], [4, 5], [7, 8]]
+matrix4 = [[1]]
+matrix5 = [[1], [2], [3], [4]]
+matrix6 = [[]]
+
+# Test outputs
+print_matrix_integer(matrix1)
+print("--")
+print_matrix_integer(matrix2)
+print("--")
+print_matrix_integer(matrix3)
+print("--")
+print_matrix_integer(matrix4)
+print("--")
+print_matrix_integer(matrix5)
+print("--")
+print_matrix_integer(matrix6)
