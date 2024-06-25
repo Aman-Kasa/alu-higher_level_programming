edace9356bf60422caac03de57454ee4313ee26c
aman <a.kasa@alustudent.com>
2024-06-25 15:52:25 -0700

    kd

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,50 +1,26 @@
 #!/usr/bin/python3
-
 import sys
-
-
-def main():
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Incorrect number of arguments. Usage: <num1> <operator> <num2>")
-        sys.exit(1)
-
-    # Extract arguments
-    num1_str, operator, num2_str = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Validate and convert num1
-    try:
-        num1 = float(num1_str)
-    except ValueError:
-        print(f"Invalid number: {num1_str}")
-        sys.exit(1)
-
-    # Validate and convert num2
-    try:
-        num2 = float(num2_str)
-    except ValueError:
-        print(f"Invalid number: {num2_str}")
-        sys.exit(1)
-
-    # Perform the calculation based on the operator
-    if operator == '+':
-        result = num1 + num2
-    elif operator == '-':
-        result = num1 - num2
-    elif operator == '*':
-        result = num1 * num2
-    elif operator == '/':
-        if num2 == 0:
-            print("Division by zero is not allowed")
-            sys.exit(1)
-        result = num1 / num2
-    else:
-        print(f"Invalid operator: {operator}")
-        sys.exit(1)
-
-    # Print the result
-    print(f"The result of {num1} {operator} {num2} is: {result}")
-
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print(f"{a} {operator} {b} = {result}")
