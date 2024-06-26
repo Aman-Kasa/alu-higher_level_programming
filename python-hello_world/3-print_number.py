f22541c376a5d74c272a37a6d51d948fb3cc6767
aman <a.kasa@alustudent.com>
2024-06-26 03:49:32 -0700

    battery avenue

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 number = 98
-
-try:
-    print(f"{number} Battery street")
-except TypeError:
-    print("ValueError: Unknown format code 'd' for object of type 'str'")
+print(f"{number:d} Battery street")
