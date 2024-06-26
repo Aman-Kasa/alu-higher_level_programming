02c54b5c02c4b8e608c3df377f3371f9864cf8cf
aman <a.kasa@alustudent.com>
2024-06-26 02:57:31 -0700

    line splited 11

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -7,15 +7,21 @@ def update_dictionary(a_dictionary, key, value):
 
 
 if __name__ == "__main__":
-    update_dictionary = __import__('7-update_dictionary').update_dictionary
-    print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+    import_name = '7-update_dictionary'
+    update_dictionary = __import__(import_name).update_dictionary
 
-    a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+    import_name = '6-print_sorted_dictionary'
+    print_sorted_dictionary = __import__(import_name).print_sorted_dictionary
+
+    a_dictionary = {
+        'language': "C",
+        'number': 89,
+        'track': "Low level"
+    }
     new_dict = update_dictionary(a_dictionary, 'language', "Python")
     print_sorted_dictionary(new_dict)
     print("--")
     print_sorted_dictionary(a_dictionary)
-
     print("--")
     print("--")
 
