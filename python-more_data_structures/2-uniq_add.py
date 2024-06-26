ee96e246766c024166670d69f2b92ffa81321f56
aman <a.kasa@alustudent.com>
2024-06-26 02:12:15 -0700

    indented trail

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,13 +4,11 @@
 def uniq_add(my_list=[]):
     unique_sum = 0
     seen_numbers = set()
-    
     for num in my_list:
         if num not in seen_numbers:
             unique_sum += num
             seen_numbers.add(num)
-    
-    return unique_sum
+        return unique_sum
 
 
 if __name__ == "__main__":
