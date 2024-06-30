240294d04d10772545eabf34b6a57243e17f089d
aman <a.kasa@alustudent.com>
2024-06-30 14:40:55 -0700

    number of main2

--- python-exceptions/2-main.py
+++ python-exceptions/2-main.py
@@ -12,3 +12,33 @@ print("nb_print: {:d}".format(nb_print))
 
 nb_print = safe_print_list_integers(my_list, 9)  # We can't use len(my_list) + 2
 print("nb_print: {:d}".format(nb_print))
+
+# Additional test cases
+my_list = [1, 2, 3, 4]
+
+nb_print = safe_print_list_integers(my_list, 4)  # Equivalent to len(my_list)
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, 2)  # Equivalent to len(my_list) - 2
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, 0)
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers([], 0)
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, 8)  # len(my_list) + 4
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, "H", 3, 4]
+nb_print = safe_print_list_integers(my_list, 5)  # Equivalent to len(my_list)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "H", 4]
+nb_print = safe_print_list_integers(my_list, 3)  # Equivalent to len(my_list) - 2
+print("nb_print: {:d}".format(nb_print))
+
+my_list = ["Holberton", 1, 2, "H", 3, 4, [1, 2, 3, 4]]
+nb_print = safe_print_list_integers(my_list, 7)  # Equivalent to len(my_list)
+print("nb_print: {:d}".format(nb_print))
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,14 +2,15 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     printed_count = 0
-    while count < x:
-        try:
-            print("{:d}".format(my_list[count]), end="")
-            printed_count += 1
-        except (ValueError, TypeError):
-            pass
-        except IndexError:
-            break
-        count += 1
+    try:
+        while count < x:
+            try:
+                print("{:d}".format(my_list[count]), end="")
+                printed_count += 1
+            except (ValueError, TypeError):
+                pass
+            count += 1
+    except IndexError:
+        pass
     print()
     return printed_count
