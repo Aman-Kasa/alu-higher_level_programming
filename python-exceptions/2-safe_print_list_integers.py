de2c80dc8268c943763d47af193a2c1ec098120d
aman <a.kasa@alustudent.com>
2024-06-30 14:44:23 -0700

    zks

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,16 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     printed_count = 0
-    try:
-        while count < x:
-            try:
-                print("{:d}".format(my_list[count]), end="")
-                printed_count += 1
-            except (ValueError, TypeError):
-                pass
-            count += 1
-    except IndexError:
-        pass
+    while count < x:
+        try:
+            print("{:d}".format(my_list[count]), end="")
+            printed_count += 1
+        except (ValueError, TypeError):
+            pass
+        except IndexError:
+            import sys
+            print("\nTraceback (most recent call last):", file=sys.stderr)
+            break
+        count += 1
     print()
     return printed_count
