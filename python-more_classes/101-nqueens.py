1e4efe746a56cd7c80e1c73e680b21bf3a6322d8
aman <a.kasa@alustudent.com>
2024-07-01 05:20:45 -0700

    qinie

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 import sys
 
+
 def print_usage_and_exit():
     print("Usage: nqueens N")
     sys.exit(1)
+
 
 def print_number_error_and_exit():
     print("N must be a number")
     sys.exit(1)
 
+
 def print_size_error_and_exit():
     print("N must be at least 4")
     sys.exit(1)
 
+
 if len(sys.argv) != 2:
     print_usage_and_exit()
+
 
 try:
     N = int(sys.argv[1])
@@ -23,6 +28,7 @@ except ValueError:
 
 if N < 4:
     print_size_error_and_exit()
+
 
 def is_safe(board, row, col):
     for i in range(col):
@@ -39,6 +45,7 @@ def is_safe(board, row, col):
 
     return True
 
+
 def solve_nqueens(board, col):
     if col >= N:
         solution = []
@@ -48,7 +55,6 @@ def solve_nqueens(board, col):
                     solution.append([i, j])
         print(solution)
         return True
-    
     res = False
     for i in range(N):
         if is_safe(board, i, col):
@@ -58,9 +64,11 @@ def solve_nqueens(board, col):
 
     return res
 
+
 def solve():
     board = [[0] * N for _ in range(N)]
     solve_nqueens(board, 0)
+
 
 if __name__ == "__main__":
     solve()
