d05ef56deab9bd47790f3c3f900a8aa69c2a59e4
aman <a.kasa@alustudent.com>
2024-07-01 13:08:20 -0700

    abett

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,24 +2,15 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-
 def main():
-    # Debug statement
-    print("Script started")
-    # Check the number of arguments
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    # Extract and convert arguments
-    try:
-        a = int(sys.argv[1])
-        operator = sys.argv[2]
-        b = int(sys.argv[3])
-    except ValueError:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    print(f"Arguments received: a={a}, operator={operator}, b={b}")
-    # Perform the operation
+    
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
@@ -31,8 +22,8 @@ def main():
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-        print(f"{a} {operator} {b} = {result}")
-
+    
+    print(f"{a} {operator} {b} = {result}")
 
 if __name__ == "__main__":
     main()
