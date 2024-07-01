5e19d250f799c0147b090eb61d897c279e12dc7e
aman <a.kasa@alustudent.com>
2024-07-01 04:36:55 -0700

    amet

--- python-exceptions/2-main.py
+++ python-exceptions/2-main.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 safe_print_list_integers = __import__('2-safe_print_list_integers').safe_print_list_integers
 
+my_list = [1, 2, 3, 4, 5]
 
-def main():
-    my_list = [1, 2, 3, 4, 5]
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
 
-    nb_print = safe_print_list_integers(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, 7)  # Changed len(my_list) to 7
+print("nb_print: {:d}".format(nb_print))
 
-    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-    nb_print = safe_print_list_integers(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
-
-
-if __name__ == "__main__":
-    main()
+nb_print = safe_print_list_integers(my_list, 9)  # Changed len(my_list) + 2 to 9
+print("nb_print: {:d}".format(nb_print))
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,17 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            pass
+            # Try to access the element at index i
+            element = my_list[i]
+            try:
+                # Try to print the element as an integer
+                print("{:d}".format(element), end="")
+                count += 1
+            except (ValueError, TypeError):
+                # Skip elements that are not integers
+                continue
         except IndexError:
+            # Break the loop if the index is out of range
             break
-    print()
+    print()  # For a new line after the loop
     return count
