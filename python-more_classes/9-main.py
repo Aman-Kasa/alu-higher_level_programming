2ada0a8d863cc6cb3683516827a1f2f356b5a110
aman <a.kasa@alustudent.com>
2024-07-01 06:03:14 -0700

    modernizn

--- python-more_classes/9-main.py
+++ python-more_classes/9-main.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
 
-# Test case 1
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
+def main():
+    try:
+        # Test case: Creating a square with size 5
+        my_square = Rectangle.square(5)
+        print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+        print(my_square)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-# Test case 2
-my_square = Rectangle.square(10)
-print("{} / {}".format(my_square.width, my_square.height))
-
-# Test case 3
-my_square = Rectangle.square(2)
-print("{} / {}".format(my_square.width, my_square.height))
-
-# Test case 4
-try:
-    my_square = Rectangle.square(-2)
-    print("{} / {}".format(my_square.width, my_square.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-# Test case 5
-try:
-    my_square = Rectangle.square("12")
-    print("{} / {}".format(my_square.width, my_square.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+if __name__ == "__main__":
+    main()
