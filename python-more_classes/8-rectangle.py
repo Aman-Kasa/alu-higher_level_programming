22969ddeb371b0803f42e1b3b219bebd881d3250
aman <a.kasa@alustudent.com>
2024-07-01 05:23:41 -0700

    qereba

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-
-"""
-Module: 8-rectangle
-Defines a Rectangle class with private width and height attributes
-"""
-
-
 class Rectangle:
-    """
-    Defines a rectangle.
-    """
-
-    number_of_instances = 0  # Class attribute to track instances
-    print_symbol = "#"  # Class attribute for string representation
+    """Defines a rectangle"""
+    number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """
-        Initializes a rectangle with optional width and height.
-        Increments number_of_instances by 1.
-        """
+        """Initializes the rectangle"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """
-        Retrieves the width of the rectangle.
-        """
+        """Retrieves the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Sets the width of the rectangle.
-        """
+        """Sets the width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -43,16 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Retrieves the height of the rectangle.
-        """
+        """Retrieves the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Sets the height of the rectangle.
-        """
+        """Sets the height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -60,47 +39,38 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """
-        Computes the area of the rectangle.
-        """
+        """Returns the area of the rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """
-        Computes the perimeter of the rectangle.
-        """
+        """Returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """
-        Returns a string representation of the rectangle.
-        """
+        """Returns the string representation of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        rect_str = ""
-        for _ in range(self.__height):
-            rect_str += str(self.print_symbol) * self.__width + "\n"
-        return rect_str.rstrip()
+        rect = (str(self.print_symbol) * self.__width + "\n") * self.__height
+        return rect[:-1]
 
     def __repr__(self):
-        """
-        Returns a string representation of the rectangle for eval().
-        """
+        """Returns the formal string representation of the rectangle"""
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        """
-        Prints a message when an instance of Rectangle is deleted.
-        Decrements number_of_instances by 1.
-        """
-        print("Bye rectangle...")
+        """Prints a message when an instance of Rectangle is deleted"""
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """
-        Returns the biggest rectangle based on the area.
-        Returns rect_1 if both have the same area value.
-        """
+        """Returns the biggest rectangle based on the area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
