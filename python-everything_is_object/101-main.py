d290509ffc0d51e4c86ab3c5503756fe874c1737
aman <a.kasa@alustudent.com>
2024-07-11 07:53:36 -0700

    resolved

--- python-everything_is_object/101-main.py
+++ python-everything_is_object/101-main.py
@@ -7,3 +7,18 @@ try:
     lc.last_name = "Snow"
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    print(lc.__dict__)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    lc.age = 30
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    lc.test = "test"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
