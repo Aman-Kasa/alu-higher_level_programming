f14b8ef0392158bb68ef575b65fd70c35dfb706d
aman <a.kasa@alustudent.com>
2024-07-11 08:03:18 -0700

    verified

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -3,9 +3,6 @@
 This module defines the LockedClass.
 """
 
+
 class LockedClass:
-    """
-    A class that prevents the user from dynamically creating new instance attributes,
-    except if the new instance attribute is called first_name.
-    """
     __slots__ = ['first_name']
