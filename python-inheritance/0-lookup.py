87ac68a299a97e12963c91ea0818a35777d0e3d0
aman <a.kasa@alustudent.com>
2024-07-12 01:57:54 -0700

    tendency

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
-# File: 0-lookup.py
+#!/usr/bin/python3
+"""
+This module defines the `lookup` function.
+"""
+
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
--- python-inheritance/0-main.py
+++ python-inheritance/0-main.py
@@ -1,5 +1,4 @@
-# File: 0-main.py
-
+#!/usr/bin/python3
 lookup = __import__('0-lookup').lookup
 
 class MyClass1(object):
@@ -10,6 +9,16 @@ class MyClass2(object):
     def my_meth(self):
         pass
 
+class MyClass3(object):
+    def dir(self):
+        return ["my_class", "is", "empty"]
+
 print(lookup(MyClass1))
 print(lookup(MyClass2))
 print(lookup(int))
+print(lookup(float))
+print(lookup(object))
+print(lookup(list))
+print(lookup(MyClass3))
+print(lookup(type('MyClass4', (), {"one_attribute": 89})))
+print(lookup(type('MyClass5', (), {"one_meth": lambda self: None})))
