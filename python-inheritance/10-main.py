7f6550f84e706218b8e72a2b2d153943dd1555b2
aman <a.kasa@alustudent.com>
2024-07-12 08:30:35 -0700

    commited number 10 square

--- python-inheritance/10-main.py
+++ python-inheritance/10-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-Main script to test Square class.
-"""
 Square = __import__('10-square').Square
 
+# Case: s = Square(13) print(s)
 s = Square(13)
-
 print(s)
+
+# Case: s = Square(13) print(s.area())
+s = Square(13)
 print(s.area())
--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,81 +1,24 @@
 #!/usr/bin/python3
-"""
-Module for Square class.
-"""
-from typing import Union
+"""Inherits BaseGeometry class"""
 
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Square class, inherits from Rectangle.
-    """
+    """Inherits from Rectangle."""
 
-    def __init__(self, size: Union[int, float]):
-        """
-        Initializes the Square instance.
-
-        Args:
-            size (int): size of the square, must be a positive integer.
-        """
-        self.__size = 0
+    def __init__(self, size):
+        """set the values"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
-    def area(self) -> int:
-        """
-        Computes the area of the square.
+    def area(self):
+        """Area of Square"""
+        return self.__size ** 2
 
-        Returns:
-            int: Area of the square.
-        """
-        return self.__size * self.__size
-
-    @property
-    def size(self) -> int:
-        """
-        Getter for size attribute.
-
-        Returns:
-            int: size of the square.
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value: Union[int, float]):
-        """
-        Setter for size attribute.
-
-        Args:
-            value (int): size of the square, must be a positive integer.
-        """
-        self.integer_validator("size", value)
-        self.__size = value
-
-    def integer_validator(self, name: str, value: Union[int, float]):
-        """
-        Validates the value as a positive integer.
-
-        Args:
-            name (str): name of the value being validated.
-            value (int): value to validate.
-
-        Raises:
-            TypeError: if value is not an integer.
-            ValueError: if value is not greater than 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be > 0")
-
-    def __str__(self) -> str:
-        """
-        Returns a string representation of the Square.
-
-        Returns:
-            str: [Square] <width>/<height>
-        """
-        return f"[Square] {self.__size}/{self.__size}"
+    def __str__(self):
+        """Returns [Square] <width>/<height>."""
+        return super().__str__()
