42d57b68a4a156a037001b804dfd5103e3675cbf
aman <a.kasa@alustudent.com>
2024-07-12 07:55:59 -0700

    trues

--- python-inheritance/tests/7-base_geometry.py
+++ python-inheritance/tests/7-base_geometry.py
@@ -1,8 +1,16 @@
+#!/usr/bin/python3
+"""Module documentation: BaseGeometry class"""
+
+
 class BaseGeometry:
+    """BaseGeometry class"""
+
     def area(self):
+        """Not yet implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validates value as an integer and greater than 0"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
--- python-inheritance/tests/7-main.py
+++ python-inheritance/tests/7-main.py
@@ -1,47 +1,22 @@
 #!/usr/bin/python3
-from 7-base_geometry import BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-def main():
-    # Case: bg = BaseGeometry() print(dir(bg))
-    bg = BaseGeometry()
-    print(dir(bg))
+bg = BaseGeometry()
 
-    # Case: bg = BaseGeometry() bg.integer_validator("myint", 12)
-    bg.integer_validator("myint", 12)
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
 
-    # Case: bg = BaseGeometry() bg.integer_validator("myint", 12) bg.integer_validator("width", 89)
-    bg.integer_validator("myint", 12)
-    bg.integer_validator("width", 89)
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-    # Case: bg = BaseGeometry() bg.integer_validator("name", "John")
-    try:
-        bg.integer_validator("name", "John")
-    except Exception as e:
-        print(f"[{e.__class__.__name__}] {e}")
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-    # Case: bg = BaseGeometry() bg.integer_validator("age", 0)
-    try:
-        bg.integer_validator("age", 0)
-    except Exception as e:
-        print(f"[{e.__class__.__name__}] {e}")
-
-    # Case: bg = BaseGeometry() bg.integer_validator("age", -4)
-    try:
-        bg.integer_validator("age", -4)
-    except Exception as e:
-        print(f"[{e.__class__.__name__}] {e}")
-
-    # Case: bg = BaseGeometry() bg.integer_validator("age", 13.5)
-    try:
-        bg.integer_validator("age", 13.5)
-    except Exception as e:
-        print(f"[{e.__class__.__name__}] {e}")
-
-    # Case: bg = BaseGeometry() bg.area()
-    try:
-        bg.area()
-    except Exception as e:
-        print(f"[{e.__class__.__name__}] {e}")
-
-if __name__ == "__main__":
-    main()
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
