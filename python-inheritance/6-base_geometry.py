bbe424d108f8adbfd10047446ac994e0e9a21c3f
aman <a.kasa@alustudent.com>
2024-07-12 02:45:50 -0700

    measures of inheritsnce

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines the `BaseGeometry` class with an `area` method.
+"""
+
+
+class BaseGeometry:
+    """A class called BaseGeo"""
+    def area(self):
+        """Raises an Exception."""
+        raise Exception('area() is not implemented')
