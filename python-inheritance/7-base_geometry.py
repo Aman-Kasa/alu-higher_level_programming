eddc79fa18a571376ee0e10a50f5cc44f689f240
aman <a.kasa@alustudent.com>
2024-07-12 08:45:11 -0700

    ddhegs

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Module documentation: BaseGeometry class"""
+'''Public instance method: def area(self):'''
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    '''Base class'''
 
     def area(self):
-        """Not yet implemented"""
+        '''raise an exception for now'''
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value as an integer and greater than 0"""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
