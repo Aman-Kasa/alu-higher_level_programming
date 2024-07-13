c14d807afb9a62e56f15b3a107fbf9778736e5d7
aman <a.kasa@alustudent.com>
2024-07-13 13:01:12 -0700

    hhush pounds

---
+++ python-classes/101-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+Square = __import__('101-square').Square
+
+# Test case 1
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+# Test case 2
+my_square = Square(5, (4, 1))
+print(my_square)
