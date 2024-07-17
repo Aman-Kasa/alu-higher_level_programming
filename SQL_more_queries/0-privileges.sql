a235e71a55daa5a8bcbdd85f53043e80a69c0831
aman <a.kasa@alustudent.com>
2024-07-17 05:41:59 -0700

    user ID

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,11 @@
 -- Check if users exist
 SELECT User, Host FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');
 
--- Show privileges for user_0d_1
+-- Show grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Show privileges for user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
--- Show user_0d_1 privileges on user_2_db database
+-- Show user_0d_1 privileges on specific database (e.g., user_2_db)
 SHOW GRANTS FOR 'user_0d_1'@'localhost' ON user_2_db;
