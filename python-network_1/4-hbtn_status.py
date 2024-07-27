6ac216b66aef234c64ebb9df779f43bc06e3cd34
Your Name <a.kasaclear>
2024-07-27 13:49:10 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Python script that fetchs."""
+"""
+This script fetches https://alu-intranet.hbtn.io/status
+and displays the body of the response.
+"""
 
 
 import requests
-if __name__ == "__main__":
-    requ = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(requ.text)))
-    print("\t- content: {}".format(requ.text))
+
+url = "https://alu-intranet.hbtn.io/status"
+response = requests.get(url)
+body = response.text
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
