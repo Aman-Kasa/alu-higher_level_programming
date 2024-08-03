9e01d305746a4c64ad6a7efb18490e5904f06a7e
aman <a.kasa@alustudent.com>
2024-08-03 12:16:51 -0700

    f

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python3
-"""
-This module contains a function that prints a name.
-"""
-
+"""A function that prints the name of a user."""
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints "My name is <first name> <last name>".
-
+    Function to take in names and print the full name.
+    
     Args:
-        first_name (str): The first name.
-        last_name (str): The last name (optional).
-
+        first_name (str): This is the first argument, and it must be a string.
+        last_name (str): This is the second argument, and it must be a string. Defaults to an empty string.
+        
     Raises:
         TypeError: If either first_name or last_name is not a string.
-
-    Examples:
-    >>> say_my_name("John", "Smith")
-    My name is John Smith
-    >>> say_my_name("Walter", "White")
-    My name is Walter White
-    >>> say_my_name("Bob")
-    My name is Bob
-    >>> try:
-    ...     say_my_name(12, "White")
-    ... except Exception as e:
-    ...     print(e)
-    first_name must be a string
-    >>> try:
-    ...     say_my_name("John", 12)
-    ... except Exception as e:
-    ...     print(e)
-    last_name must be a string
+        
+    Returns:
+        None
+        
+    Prints:
+        My name is <first name> <last name>
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
