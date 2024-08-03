a132f36690273a4976cba760b027d5db05c453e3
aman <a.kasa@alustudent.com>
2024-08-03 12:34:43 -0700

    3rd

--- python-test_driven_development/3-main.py
+++ python-test_driven_development/3-main.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
+import importlib.util
+import sys
 
+module_name = "3-say_my_name"
+file_path = "./3-say_my_name.py"
+
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+module = importlib.util.module_from_spec(spec)
+sys.modules[module_name] = module
+spec.loader.exec_module(module)
+say_my_name = module.say_my_name
+
+# Test cases
 say_my_name("John", "Smith")
 say_my_name("Walter", "White")
 say_my_name("Bob")
 try:
     say_my_name(12, "White")
+except Exception as e:
+    print(e)
+try:
+    say_my_name("John", 12)
+except Exception as e:
+    print(e)
+try:
+    say_my_name()
 except Exception as e:
     print(e)
