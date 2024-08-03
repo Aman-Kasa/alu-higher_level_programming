eb52d0af4aaf20547e71ce31ab35e7cca825ba67
aman <a.kasa@alustudent.com>
2024-08-03 12:54:11 -0700

    tests run

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,27 +1,50 @@
 #!/usr/bin/python3
-"""_summary_
-Write a function that prints My name is <first name> <last name>
-"""
-
+"""A function that prints the name of a user."""
 
 def say_my_name(first_name, last_name=""):
-    """_summary_
-
-    Args:
-        first_name (_type_): _User first name_
-        last_name (str, optional): _User last name_. Defaults to "".
-
-    Raises:
-        TypeError: _if first_name is not a string_
-        TypeError: _if last_name is not a string_
-
-    Returns:
-        _str_: _My name is {first_name} {last_name}_
     """
-
+    Function to take in names and print the full name.
+    
+    Args:
+        first_name (str): This is the first argument, and it must be a string.
+        last_name (str): This is the second argument, and it must be a string. Defaults to an empty string.
+        
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
+        
+    Returns:
+        None
+        
+    Prints:
+        My name is <first name> <last name>
+    
+    Examples:
+        >>> say_my_name("John", "Smith")
+        My name is John Smith
+        >>> say_my_name("Walter", "White")
+        My name is Walter White
+        >>> say_my_name("Bob")
+        My name is Bob 
+        >>> say_my_name(12, "White")
+        Traceback (most recent call last):
+            ...
+        TypeError: first_name must be a string
+        >>> say_my_name("John", 12)
+        Traceback (most recent call last):
+            ...
+        TypeError: last_name must be a string
+        >>> say_my_name()
+        Traceback (most recent call last):
+            ...
+        TypeError: missing 1 required positional argument: 'first_name'
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}")
+    print("My name is {} {}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
