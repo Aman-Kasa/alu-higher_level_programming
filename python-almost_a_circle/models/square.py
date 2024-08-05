afc95949f11bea378ca9c4a4f8dfda96aa211abd
aman <a.kasa@alustudent.com>
2024-08-05 11:28:00 -0700

    11m

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
-"""Defines a square class."""
+"""Module that defines the Square class."""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square, inheriting from Rectangle."""
-
+    """Class Square that inherits from Rectangle."""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes a new Square instance."""
+        """Initialize a new Square instance."""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return (
-            f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-        )
+        """Return a string representation of the Square instance."""
+        return ("[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+        ))
+
+    @property
+    def size(self):
+        """Get the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the Square."""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update attributes of the Square instance."""
+        if args and len(args) > 0:
+            attr_names = ['id', 'size', 'x', 'y']
+            for attr, val in zip(attr_names, args):
+                setattr(self, attr, val)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
