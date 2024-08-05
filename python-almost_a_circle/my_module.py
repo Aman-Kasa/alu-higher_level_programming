65a79ff96d498d732bdf2e38fa0853721993e9fd
aman <a.kasa@alustudent.com>
2024-08-04 18:05:10 -0700

    almost a circle first test

---
+++ python-almost_a_circle/my_module.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module provides an example class and functions.
+"""
+
+
+class MyClass:
+    """
+    This is an example class.
+    """
+
+    def my_method(self):
+        """
+        This method does something.
+        """
+        pass
+
+
+def my_function():
+    """
+    This function does something.
+    """
+    pass
