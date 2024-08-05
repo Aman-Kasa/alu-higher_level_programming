ad07fff175f829c81b1de8b5ec1e079b8a94d94d
aman <a.kasa@alustudent.com>
2024-08-05 13:09:25 -0700

    Added CSV serialization and deserialization methods to Base class, and tested with 18-main.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the Base class."""
 import json
+import csv
 import os
 
 
@@ -64,4 +65,37 @@ class Base:
 
         with open(filename, "r") as jsonfile:
             list_dicts = cls.from_json_string(jsonfile.read())
+            return [cls.create(**d) for d in list_dicts]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Writes the CSV string representation of list_objs to a file."""
+        filename = "{}.csv".format(cls.__name__)
+        with open(filename, "w", newline='') as csvfile:
+            if list_objs is None:
+                csvfile.write("[]")
+            else:
+                fieldnames = []
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == "Square":
+                    fieldnames = ['id', 'size', 'x', 'y']
+                
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Returns a list of instances from a CSV file."""
+        filename = "{}.csv".format(cls.__name__)
+        if not os.path.exists(filename):
+            return []
+
+        with open(filename, "r", newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            list_dicts = [
+                dict((k, int(v)) for k, v in row.items()) for row in reader
+            ]
             return [cls.create(**d) for d in list_dicts]
