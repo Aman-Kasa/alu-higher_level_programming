5dac596641afa7d78af975fcf16e56ffcdb086db
aman <a.kasa@alustudent.com>
2024-08-05 12:09:51 -0700

    2mm

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,55 +9,98 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle instance"""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
-        """Get the width of the rectangle"""
+        """Get the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle"""
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError("width must be a positive integer")
+        """Set the width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        """Get the height of the rectangle"""
+        """Get the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle"""
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError("height must be a positive integer")
+        """Set the height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
-        """Get the x coordinate of the rectangle"""
+        """Get the x coordinate"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Set the x coordinate of the rectangle"""
-        if not isinstance(value, int) or value < 0:
-            raise ValueError("x must be a non-negative integer")
+        """Set the x coordinate with validation"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """Get the y coordinate of the rectangle"""
+        """Get the y coordinate"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Set the y coordinate of the rectangle"""
-        if not isinstance(value, int) or value < 0:
-            raise ValueError("y must be a non-negative integer")
+        """Set the y coordinate with validation"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """Print the rectangle with the # character"""
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """Return a string representation of the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """Update the Rectangle instance with and/or keyword arguments"""
+        if args:
+            attrs = ['id', 'width', 'height', 'x', 'y']
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle"""
+        return {
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
+        }
