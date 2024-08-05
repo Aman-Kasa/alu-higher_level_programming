a7cf09fc39e5e0493e07a282fbcbecffca1209fb
aman <a.kasa@alustudent.com>
2024-08-05 13:18:44 -0700

    Add Rectangle class that inherits from Base with appropriate attributes and methods

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Defines the Rectangle class."""
+"""Defines a rectangle class."""
 from models.base import Base
 
 
 class Rectangle(Base):
     """Represents a rectangle."""
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x coordinate of the rectangle.
+            y (int): The y coordinate of the rectangle.
+            id (int): The identity of the rectangle.
         """
         super().__init__(id)
         self.width = width
@@ -21,38 +20,42 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    # ... other methods ...
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
 
-    def to_dictionary(self):
-        """Returns the dictionary representation of a Rectangle."""
-        return {
-            'id': self.id,
-            'width': self.width,
-            'height': self.height,
-            'x': self.x,
-            'y': self.y
-        }
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        self.__width = value
 
-    def update(self, *args, **kwargs):
-        """Update the Rectangle.
-        Args:
-            *args (ints): New attribute values.
-            **kwargs (dict): New key/value pairs of attributes.
-        """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    self.id = arg
-                elif a == 1:
-                    self.width = arg
-                elif a == 2:
-                    self.height = arg
-                elif a == 3:
-                    self.x = arg
-                elif a == 4:
-                    self.y = arg
-                a += 1
-        elif kwargs and len(kwargs) != 0:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x coordinate of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x coordinate of the rectangle."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y coordinate of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y coordinate of the rectangle."""
+        self.__y = value
