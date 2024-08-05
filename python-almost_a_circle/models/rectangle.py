ea65cda08526741985f23347dcfa5e6ce6e64560
aman <a.kasa@alustudent.com>
2024-08-05 03:16:45 -0700

    12th main.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,90 +1,40 @@
-#!/usr/bin/python3
-"""Defines the Rectangle class."""
-from models.base import Base
+# models/rectangle.py
 
 
-class Rectangle(Base):
-    """Represents a rectangle."""
+class Rectangle:
+    """Rectangle class definition"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes the rectangle."""
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        self.id = id
+        # Add validation and other initializations as needed
 
-    @property
-    def width(self):
-        return self.__width
+    # Add other methods and properties as needed
 
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """Returns the area of the rectangle."""
-        return self.width * self.height
-
-    def display(self):
-        """Prints the rectangle with the character #."""
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
+    
     def __str__(self):
-        """Returns a string representation of the rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
-
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+    
     def update(self, *args, **kwargs):
-        """Updates the attributes of the rectangle."""
+        """Update method to handle args and kwargs"""
+        attrs = ['id', 'width', 'height', 'x', 'y']
         if args:
-            attributes = ["id", "width", "height", "x", "y"]
-            for index, value in enumerate(args):
-                if index < len(attributes):
-                    setattr(self, attributes[index], value)
-        else:
+            for i, value in enumerate(args):
+                if i < len(attrs):
+                    setattr(self, attrs[i], value)
+        elif kwargs:
             for key, value in kwargs.items():
-                if hasattr(self, key):
+                if key in attrs:
                     setattr(self, key, value)
