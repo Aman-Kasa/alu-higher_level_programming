f89d800757e9895816c22236776284a0d7195b4a
aman <a.kasa@alustudent.com>
2024-08-05 07:46:37 -0700

    0main

--- python-almost_a_circle/0-main.py
+++ python-almost_a_circle/0-main.py
@@ -5,16 +5,16 @@ from models.base import Base
 if __name__ == "__main__":
 
     b1 = Base()
-    print(b1.id)
+    print(b1.id)  # Expected output: 1
 
     b2 = Base()
-    print(b2.id)
+    print(b2.id)  # Expected output: 2
 
     b3 = Base()
-    print(b3.id)
+    print(b3.id)  # Expected output: 3
 
     b4 = Base(12)
-    print(b4.id)
+    print(b4.id)  # Expected output: 12
 
     b5 = Base()
-    print(b5.id)
+    print(b5.id)  # Expected output: 4
