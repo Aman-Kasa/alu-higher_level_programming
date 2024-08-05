b60a0955c167a151d2aa2fe4abb902e48b71c6ac
aman <a.kasa@alustudent.com>
2024-08-05 11:39:30 -0700

    12main

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
+"""Module defining a Rectangle class"""
+
+
 class Rectangle:
-    """Class Rectangle."""
+    """Class representing a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle instance."""
+        """Initialize a new Rectangle instance"""
         self.width = width
         self.height = height
         self.x = x
@@ -9,13 +13,13 @@ class Rectangle:
         self.id = id
 
     def __str__(self):
-        """Return a string representation of the Rectangle instance."""
+        """Return a string representation of the Rectangle instance"""
         return "[Rect] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height
         )
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Rectangle."""
+        """Return the dictionary representation of a Rectangle"""
         return {
             'id': self.id,
             'width': self.width,
@@ -25,7 +29,7 @@ class Rectangle:
         }
 
     def update(self, *args, **kwargs):
-        """Update the Rectangle instance with  arguments."""
+        """Update the Rectangle instance  arguments and/or  arguments"""
         if args:
             attrs = ['id', 'width', 'height', 'x', 'y']
             for attr, value in zip(attrs, args):
