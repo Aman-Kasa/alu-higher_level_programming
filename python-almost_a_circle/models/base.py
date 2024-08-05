7000db058d2853fac0ce36ce0be27672cc39d63a
aman <a.kasa@alustudent.com>
2024-08-05 12:26:46 -0700

    14main.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-"""Module defining the Base class."""
+"""Module defining a Base class"""
+
+import json
 
 
 class Base:
-    """Base class for managing id attributes in future classes."""
+    """Base class for managing ID"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a Base instance.
-
-        Args:
-            id (int): The ID of the instance. If None, assigns a new ID.
-        """
-        if id is not None:
-            self.id = id
-        else:
+        """Initialize a new Base instance"""
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+        else:
+            self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries (list of dict): List of dictionaries to convert
+
+        Returns:
+            str: JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
