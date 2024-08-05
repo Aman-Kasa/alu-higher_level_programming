50d3103d4ace20e90ed68b1aa2ce38ea1b5b920d
aman <a.kasa@alustudent.com>
2024-08-05 03:20:05 -0700

    13th main.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,32 +4,44 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a Square instance"""
         super().__init__(size, size, x, y, id)
-    
+        self.size = size
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
     @property
     def size(self):
+        """Getter for size"""
         return self.width
-    
+
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """Setter for size"""
         self.width = value
         self.height = value
-    
-    def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    
+
     def update(self, *args, **kwargs):
+        """Update Square attributes"""
+        attrs = ['id', 'size', 'x', 'y']
         if args:
-            attrs = ['id', 'size', 'x', 'y']
             for i, value in enumerate(args):
                 if i < len(attrs):
                     setattr(self, attrs[i], value)
         elif kwargs:
             for key, value in kwargs.items():
-                if key in ['id', 'size', 'x', 'y']:
+                if key in attrs:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square"""
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
