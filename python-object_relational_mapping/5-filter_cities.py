51408696e42aa21ada1372a08fa0056d18dc6cd5
aman <a.kasa@alustudent.com>
2024-08-06 06:28:01 -0700

    filtwr

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A script that lists all cities of a specified state from the database 
+A script that lists all cities of a specified state from the database
 hbtn_0e_4_usa. Results are sorted in ascending order by cities.id.
 """
 
@@ -10,8 +10,8 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./5-filter_cities.py <mysql username> <mysql password> "
-              "<database name> <state name>")
+        print("Usage: ./5-filter_cities.py <mysql username> "
+              "<mysql password> <database name> <state name>")
         sys.exit(1)
 
     mysql_username = sys.argv[1]
@@ -29,26 +29,21 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    # Query to get the state id based on the state name
-    query = "SELECT id FROM states WHERE name = %s"
+    # Use a single execute() call with JOIN to get all the necessary data
+    query = (
+        "SELECT cities.name FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
+    )
     cursor.execute(query, (state_name,))
-    state_id = cursor.fetchone()
-
-    if state_id is None:
-        print()
-        cursor.close()
-        db.close()
-        sys.exit(0)
-
-    state_id = state_id[0]
-
-    # Query to get cities for the state_id
-    query = "SELECT name FROM cities WHERE state_id = %s ORDER BY id ASC"
-    cursor.execute(query, (state_id,))
     results = cursor.fetchall()
 
     # Print cities in a comma-separated format
-    print(", ".join(row[0] for row in results))
+    if results:
+        print(", ".join(row[0] for row in results))
+    else:
+        print()
 
     cursor.close()
     db.close()
