11b68e039300990e93ab85c9b027ddca339b3899
aman <a.kasa@alustudent.com>
2024-08-06 06:12:05 -0700

    2.py object relational

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import MySQLdb
 import sys
 
@@ -6,7 +11,7 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: ./2-my_filter_states.py <mysql username> "
-              "<mysql password> <db name> <state name>")
+              "<mysql password> <database name> <state name>")
         sys.exit(1)
 
     mysql_username = sys.argv[1]
@@ -14,13 +19,18 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                         passwd=mysql_password, db=db_name)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name
+    )
 
     cursor = db.cursor()
 
-    query = ("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-             .format(state_name))
+    query = ("SELECT * FROM states WHERE name = '{}' "
+             "ORDER BY id ASC".format(state_name))
 
     cursor.execute(query)
     results = cursor.fetchall()
