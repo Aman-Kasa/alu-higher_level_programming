1e8917c5be08a9732b38b2fe69241589e9ac40dd
aman <a.kasa@alustudent.com>
2024-08-06 06:40:45 -0700

    5.py script

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,53 +1,39 @@
 #!/usr/bin/python3
-"""
-A script that lists all cities of a specified state from the database
-hbtn_0e_4_usa. Results are sorted in ascending order by cities.id.
-"""
+'''
+    A script that takes in the name of a state as an
+    argument and lists all cities of that state, using
+    the database hbtn_0e_4_usa
+'''
 
-import MySQLdb
+
 import sys
-
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: ./5-filter_cities.py <mysql username> <mysql password> "
-              "<database name> <state name>")
-        return
-
-    mysql_user = sys.argv[1]
-    mysql_pass = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_user,
-            passwd=mysql_pass,
-            db=db_name
-        )
-        cursor = db.cursor()
-        
-        query = (
-            "SELECT cities.name FROM cities "
-            "JOIN states ON cities.state_id = states.id "
-            "WHERE states.name = %s "
-            "ORDER BY cities.id ASC"
-        )
-        cursor.execute(query, (state_name,))
-        rows = cursor.fetchall()
-        
-        if rows:
-            print(", ".join(row[0] for row in rows))
-        else:
-            print()
-
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
+import MySQLdb
 
 
 if __name__ == "__main__":
-    main()
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
+
+    state_name = sys.argv[4]
+
+    cursor = conn.cursor()
+
+    sql_com = """SELECT cities.name
+          FROM states
+          INNER JOIN cities ON states.id = cities.state_id
+          WHERE states.name = %s
+          ORDER BY cities.id ASC"""
+
+    cursor.execute(sql_com, (state_name,))
+
+    data = cursor.fetchall()
+
+    print(", ".join([city[0] for city in data]))
+
+    cursor.close()
+    conn.close()
