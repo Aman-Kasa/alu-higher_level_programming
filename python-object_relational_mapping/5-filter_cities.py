4030106c866ba589dd0f13197f370fcadf86c92f
aman <a.kasa@alustudent.com>
2024-08-06 06:34:46 -0700

    5.filter .py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,40 +8,48 @@ import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 4:
-        print("Usage: ./5-filter_cities.py <mysql username> "
-              "<mysql password> <database name> <state name>")
-        sys.exit(1)
+        print("Usage: ./5-filter_cities.py <mysql username> <mysql password> "
+              "<database name> <state name>")
+        return
 
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
-    )
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name
+        )
 
-    cursor = db.cursor()
+        cursor = db.cursor()
 
-    query = (
-        "SELECT cities.name FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
-        "ORDER BY cities.id ASC"
-    )
-    cursor.execute(query, (state_name,))
-    results = cursor.fetchall()
+        query = (
+            "SELECT cities.name FROM cities "
+            "JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %s "
+            "ORDER BY cities.id ASC"
+        )
+        cursor.execute(query, (state_name,))
+        results = cursor.fetchall()
 
-    if results:
-        print(", ".join(city[0] for city in results))
-    else:
-        print()
+        if results:
+            print(", ".join(city[0] for city in results))
+        else:
+            print()
 
-    cursor.close()
-    db.close()
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as err:
+        print(f"Error: {err}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
