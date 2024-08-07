956a31b85021421d0e570cd59a33048836bdc0fa
Aman-Kasa <a.kasa@alustudent.com>
2024-08-07 14:27:24 -0700

    square inheritng from rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""" Square Module """
+""" Module that contains the class Square, inheriting from Rectangle """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Square class inherits from Rectangle """
+    """ Class representing a Square, inheriting from Rectangle """    
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initialize a new Square instance """
+        """ Initializes the Square """
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    def __str__(self):
+        """ Returns a string representation of the Square """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
@@ -17,27 +21,6 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """ Setter for size """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """ Setter for size, updating width and height """
         self.width = value
         self.height = value
-
-    def __str__(self):
-        """ Return the string representation of the Square """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
-
-    def update(self, *args, **kwargs):
-        """ Update attributes """
-        if args:
-            attributes = ["id", "size", "size", "x", "y"]
-            for attr, value in zip(attributes, args):
-                setattr(self, attr, value)
-        else:
-            for key, value in kwargs.items():
-                if key == 'size':
-                    self.size = value
-                elif hasattr(self, key):
-                    setattr(self, key, value)
