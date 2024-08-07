3b185df6879978405a952dd22f0951ff2ad4f4f8
Aman-Kasa <a.kasa@alustudent.com>
2024-08-07 15:24:47 -0700

    14please bae

--- python-almost_a_circle/14-main.py
+++ python-almost_a_circle/14-main.py
@@ -2,13 +2,16 @@
 """ 14-main """
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 if __name__ == "__main__":
     r1 = Rectangle(10, 7, 2, 8)
-    dictionary = r1.to_dictionary()
-    json_dictionary = Base.to_json_string([dictionary])
-    print(dictionary)
-    print(type(dictionary))
+    s1 = Square(5, 3, 6)
+    dictionary_r1 = r1.to_dictionary()
+    dictionary_s1 = s1.to_dictionary()
+    json_dictionary = Base.to_json_string([dictionary_r1, dictionary_s1])
+    print(dictionary_r1)
+    print(dictionary_s1)
     print(json_dictionary)
     print(type(json_dictionary))
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,48 +1,19 @@
 #!/usr/bin/python3
-""" Module that contains the class Square """
+""" Module that contains the Square class """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Class representing a Square, inheriting from Rectangle """
+    """ Class representing a Square """
     def __init__(self, size, x=0, y=0, id=None):
         """ Initializes the Square """
         super().__init__(size, size, x, y, id)
-        self.size = size
-
-    @property
-    def size(self):
-        """ Getter for size """
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """ Setter for size with validation """
-        self.width = value
-        self.height = value
-
-    def __str__(self):
-        """ Returns a string representation of the Square """
-        return ("[Square] ({}) {}/{} - {}"
-                .format(self.id, self.x, self.y, self.size))
-
-    def update(self, *args, **kwargs):
-        """ Updates the Square attributes """
-        if args:
-            attributes = ['id', 'size', 'x', 'y']
-            for i, value in enumerate(args):
-                if i < len(attributes):
-                    setattr(self, attributes[i], value)
-        else:
-            for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """ Returns the dictionary representation of a Square """
+        """ Returns a dictionary representation of the Square """
         return {
             'id': self.id,
-            'size': self.size,
+            'size': self.width,
             'x': self.x,
             'y': self.y
         }
