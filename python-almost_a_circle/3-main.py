b999aad28067f03252a8236062d7fe5a2628b130
Aman-Kasa <a.kasa@alustudent.com>
2024-08-07 16:42:00 -0700

    mai.py arrangement

--- python-almost_a_circle/3-main.py
+++ python-almost_a_circle/3-main.py
@@ -2,14 +2,14 @@
 """ 3-main """
 from models.rectangle import Rectangle
 
-
 if __name__ == "__main__":
 
+    # Create instances of Rectangle and print their areas
     r1 = Rectangle(3, 2)
-    print(r1.area())
+    print(r1.area())  # Expected output: 6
 
     r2 = Rectangle(2, 10)
-    print(r2.area())
+    print(r2.area())  # Expected output: 20
 
     r3 = Rectangle(8, 7, 0, 0, 12)
-    print(r3.area())
+    print(r3.area())  # Expected output: 56
