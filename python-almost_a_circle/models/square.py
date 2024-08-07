3128715ef32c9f5e35e2d6bf2e294671605c3787
Aman-Kasa <a.kasa@alustudent.com>
2024-08-07 14:53:11 -0700

    back to square again ./13

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module that contains the class Square, inheriting from Rectangle """
+""" Module that contains the class Square """
 from models.rectangle import Rectangle
 
 
@@ -8,11 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Initializes the Square """
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """ Returns a string representation of the Square """
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+        self.size = size
 
     @property
     def size(self):
@@ -21,13 +17,18 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """ Setter for size, updating width and height with validation """
+        """ Setter for size with validation """
         self.width = value
         self.height = value
 
+    def __str__(self):
+        """ Returns a string representation of the Square """
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.size))
+
     def update(self, *args, **kwargs):
         """ Updates the Square attributes """
-        if args and len(args) > 0:
+        if args:
             attributes = ['id', 'size', 'x', 'y']
             for i, value in enumerate(args):
                 if i < len(attributes):
@@ -36,3 +37,12 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns the dictionary representation of a Square """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
