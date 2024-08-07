8e1d43ce1b288d08f5b3b316e01278c282dba807
Aman-Kasa <a.kasa@alustudent.com>
2024-08-07 02:34:14 -0700

    0-base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,13 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ Base class module """
 
-
 class Base:
+    """ Base class to manage id attribute """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Initialize Base instance """
         if id is not None:
             self.id = id
         else:
