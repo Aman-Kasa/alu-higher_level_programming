4d416e2d5abcc5af8146769023a217c2eb6f0928
Aman-Kasa <a.kasa@alustudent.com>
2024-08-07 15:34:40 -0700

    added docs

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,11 +3,20 @@ import json
 
 
 class Base:
-    """ A base class for other classes in the project """
+    """A base class for other classes in the project.
+
+    This class manages id attribute and provides utility methods
+    for JSON serialization.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ Initialize a new Base instance """
+        """Initialize a new Base instance.
+
+        Args:
+            id (int): The id to set for the instance. If None, a new id
+                      will be automatically assigned.
+        """
         if id is not None:
             self.id = id
         else:
@@ -16,7 +25,16 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Convert a list of dictionaries to a JSON string """
+        """Convert a list of dictionaries to a JSON string.
+
+        Args:
+            list_dictionaries (list of dict): The list of dictionaries to
+                                              serialize to JSON.
+
+        Returns:
+            str: The JSON string representation of the list of dictionaries.
+                 If list_dictionaries is None or empty, returns "[]".
+        """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
