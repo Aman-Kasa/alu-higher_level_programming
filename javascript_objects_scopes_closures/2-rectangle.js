2acb28e2c06387129ddba58ee74436eea0cb2172
Aman-Kasa <a.kasa@alustudent.com>
2024-09-19 12:33:54 +0200

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,12 @@
 
 class Rectangle {
   constructor (w, h) {
+    // Initialize width and height only if w and h are positive integers
     if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
+      // Set width and height to undefined if invalid input
       this.width = undefined;
       this.height = undefined;
     }
