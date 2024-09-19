fc8583c6cd41d4743e862f6344f3b5bc2cd7eccc
Aman-Kasa <a.kasa@alustudent.com>
2024-09-19 12:41:09 +0200

    Update 2-main.js

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -1,18 +1,7 @@
 #!/usr/bin/node
-
 const Rectangle = require('./2-rectangle');
 
-const r1 = new Rectangle(2, 3);
-console.log(`Instance width: ${r1.width} - height: ${r1.height}`);
-
 const r2 = new Rectangle(2, -3);
-console.log(`Instance width: ${r2.width} - height: ${r2.height}`);
-
-const r3 = new Rectangle(3);
-console.log(`Instance width: ${r3.width}`);
-
-const r4 = new Rectangle(2, 0);
-console.log(`Instance width: ${r4.width} - height: ${r4.height}`);
-
-const r5 = new Rectangle();
-console.log(`Instance width: ${r5.width} - height: ${r5.height}`);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
