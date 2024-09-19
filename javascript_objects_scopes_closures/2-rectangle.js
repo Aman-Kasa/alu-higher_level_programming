f6d94ffb77d5d0f2ef3eed91c6ce1f1720b2f9d1
Aman-Kasa <a.kasa@alustudent.com>
2024-09-19 12:29:15 +0200

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+  constructor (w, h) {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
