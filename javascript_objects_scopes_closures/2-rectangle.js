cb53e4afe2e9a8d86697f6a7a28d8a279cb907dd
aman <a.kasa@alustudent.com>
2024-09-19 02:53:21 -0700

    0-upto 10

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+}
+
+module.exports = Rectangle;
