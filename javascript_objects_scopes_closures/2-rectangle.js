322bbc6e6f7c4afa217c26cd1726a8d0d251b322
aman <a.kasa@alustudent.com>
2024-09-24 01:47:33 -0700

    2-rec

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,15 +2,12 @@
 
 class Rectangle {
   constructor (w, h) {
-    // Check if w and h are positive integers
-    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+    // Check if both w and h are positive integers
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      // Initialize with default values if conditions are not met
-      this.width = undefined;
-      this.height = undefined;
     }
+    // Otherwise, do nothing (creates an empty object)
   }
 }
 
