2e31deb9c2e44690c8f827def0489069dd621efd
aman <a.kasa@alustudent.com>
2024-09-24 01:40:53 -0700

    101

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,23 +1,14 @@
 #!/usr/bin/node
 
-// Import the dictionary from 101-data.js
 const dict = require('./101-data.js').dict;
 
-// Create an empty dictionary to store occurrences
 const newDict = {};
 
-// Loop through each key-value pair in the original dictionary
-for (const userId in dict) {
-  const occurrence = dict[userId];  // Get the number of occurrences for each user id
-  
-  // If the occurrence is not already a key in newDict, add it
-  if (!newDict[occurrence]) {
-    newDict[occurrence] = [];
+for (const [userId, occurrences] of Object.entries(dict)) { // Get the number of occurrences and user ids
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
   }
-  
-  // Push the user id into the list of the corresponding occurrence
-  newDict[occurrence].push(userId);
+  newDict[occurrences].push(userId);
 }
 
-// Print the new dictionary
 console.log(newDict);
