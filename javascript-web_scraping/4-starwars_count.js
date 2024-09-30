0aa4f1f197f97992ca7ef4dba6a9f943eaf132d4
aman <a.kasa@alustudent.com>
2024-09-30 09:28:00 -0700

    4-starwars_count.js

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Get the API URL from the command-line argument
+const apiUrl = process.argv[2];
+
+// Make a GET request to the Star Wars API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error); // Print the error if any
+  } else {
+    const films = JSON.parse(body).results; // Parse the JSON response
+    const wedgeId = '18';
+    let count = 0;
+
+    // Loop through each film and check if Wedge Antilles (ID 18) is present
+    for (const film of films) {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeId}/`)) {
+        count++;
+      }
+    }
+    console.log(count); // Print the count of movies Wedge Antilles appears in
+  }
+});
