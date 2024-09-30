0a7a059f6a1ad15d63c277152dee1a9d194d7767
aman <a.kasa@alustudent.com>
2024-09-30 08:55:04 -0700

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -7,8 +7,8 @@ const filePath = process.argv[2];
 // Read the file content in UTF-8
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);  // Print the error object if an error occurs
+    console.log(err); // Print the error object if an error occurs
   } else {
-    console.log(data);  // Print the file content if no error occurs
+    console.log(data); // Print the file content if no error occurs
   }
 });
